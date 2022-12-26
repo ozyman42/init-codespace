b7ea8f9d1c70d63b8ca73ef76eb7ed00ad9e8c09
Ozymandias <alex.l.leung@gmail.com>
2022-12-26 21:06:42 +0000

    yes to rust installation

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo "hello world" > ~/hello.txt
 sudo apt-get update
 sudo apt-get install tmux -y
 npm i -g pnpm
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 node -e "p=require('path');f=require('fs');let l=p.resolve('/home/codespace/.vscode-remote/data/Machine/settings.json'); \
         let c=JSON.parse(f.readFileSync(l).toString());c['workbench.colorTheme']='Default Dark+'; \
         fs.writeFileSync(l, JSON.stringify(c, null, 2))"

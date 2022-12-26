9f6a5ae78c6edc32546658ff5d7745a6c626c229
Ozymandias <alex.l.leung@gmail.com>
2022-12-26 09:19:17 +0000

    my settings

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install tmux -y
+npm i -g pnpm
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+node -e "p=require('path');f=require('fs');let l=p.resolve('/home/codespace/.vscode-remote/data/Machine/settings.json'); \
+        let c=JSON.parse(f.readFileSync(l).toString());c['workbench.colorTheme']='Default Dark+'; \
+        fs.writeFileSync(l, JSON.stringify(c, null, 2))"

c428842a3e241e2e643e2da0c9e9aa07bce69dd7
Ozymandias <alex.l.leung@gmail.com>
2023-03-04 23:06:31 +0000

    Auto yes to all prompts and delete all extra file/dirs

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ node -e "p=require('path');f=require('fs');let l=p.resolve('/home/codespace/.vsc
         let c=JSON.parse(f.readFileSync(l).toString());c['workbench.colorTheme']='Default Dark+'; \
         fs.writeFileSync(l, JSON.stringify(c, null, 2))"
 # https://next--tauri.netlify.app/next/guides/getting-started/prerequisites/linux
-sudo apt install libwebkit2gtk-4.0-dev \
+sudo apt install -y libwebkit2gtk-4.0-dev \
     build-essential \
     libssl-dev \
     libgtk-3-dev \
@@ -15,13 +15,16 @@ sudo apt install libwebkit2gtk-4.0-dev \
     librsvg2-dev
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
 # Standalone install
-sudo apt install default-jdk
+sudo apt install -y default-jdk
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 wget https://dl.google.com/android/repository/commandlinetools-linux-8512546_latest.zip -O cmdline-tools.zip
 unzip cmdline-tools.zip
-mkdir ~/.android/cmdline-tools/latest
+mkdir -p ~/.android/cmdline-tools/latest
 mv cmdline-tools/* ~/.android/cmdline-tools/latest
-rm cmdline-tools
+rm -d cmdline-tools
+rm cmdline-tools.zip
 export ANDROID_HOME="$HOME/.android"
 export NDK_HOME="$ANDROID_HOME/ndk/25.0.8775105"
-~/.android/cmdline-tools/latest/bin/sdkmanager "platforms;android-33" "platform-tools" "ndk;25.0.8775105" "build-tools;33.0.0"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin/"
+yes | sdkmanager --licenses
+sdkmanager "platforms;android-33" "platform-tools" "ndk;25.0.8775105" "build-tools;33.0.0"
